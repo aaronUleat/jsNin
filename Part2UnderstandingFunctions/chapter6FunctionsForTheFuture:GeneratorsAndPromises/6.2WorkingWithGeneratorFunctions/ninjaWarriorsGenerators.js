0f15fd75ac745fb8ad1363c2528888245ff8aa0b
aaronuleat <aaronuleat@gmail.com>
2018-07-29 10:27:43 -0600

    commit

--- Part2UnderstandingFunctions/chapter6FunctionsForTheFuture:GeneratorsAndPromises/6.2WorkingWithGeneratorFunctions/ninjaWarriorsGenerators.js
+++ Part2UnderstandingFunctions/chapter6FunctionsForTheFuture:GeneratorsAndPromises/6.2WorkingWithGeneratorFunctions/ninjaWarriorsGenerators.js
@@ -3,9 +3,20 @@ function assert(value, description) {
     console.log(result + ' - ' +  description);
 }
 
-function* WarriorGenerator () {
-    yield ""
+function* WeaponGenerator(){
+	yield "Katana";
+	yield "Wakizashi";
 }
+// Se crea un iterador
+const weaponsIterator = WeaponGenerator();
+// Se crea una variable donde vamos a guardar los items de la secuencia generada.
+let item;
+// En cada loop de la iteracion se trae un valor del generador y hace output del valor.
+// Se detiene el iterador cuando el generador no tenga mas valores ha producir.
+while(!(item = weaponsIterator.next()).done) {
+	assert(item !== null, item.value);
+}
+
 
 
 
